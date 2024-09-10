7a6a75b4c0b918cf4dfa0ae2b92c73c2f0ace11e
Mustafa YAĞIZ <112881823+myagjz@users.noreply.github.com>
2024-09-10 12:04:41 +0300

    Add files via upload

---
+++ ft_printf/ft_printf.c
@@ -0,0 +1,42 @@
+#include <unistd.h>
+#include <stdarg.h>
+
+int putstr(char *str, int i)
+{
+	if (!str)
+		return putstr("(null)", i);
+	if (!str[i])
+		return i;
+	write(1, &str[i], 1);
+	return putstr(str, i+1);
+}
+
+int putnum(long num, int base)
+{
+	char *b = "0123456789abcdef";
+	if (num < 0)
+		return write(1, "-", 1) + putnum(num * -1, base);
+	if (num / base == 0)
+		return write(1, &b[num % base], 1);
+	return putnum(num / base, base) + putnum(num % base, base);
+}
+
+int ptf(char *s, va_list args, int i, int n)
+{
+	if (!s[i])
+		return n;
+	if (s[i] == '%' && s[i + 1] == 's')
+		return ptf(s, args, i + 2, n + putstr(va_arg(args, char *), 0));
+	if (s[i] == '%' && s[i + 1] == 'd')
+		return ptf(s, args, i + 2, n + putnum(va_arg(args, int), 10));
+	if (s[i] == '%' && s[i + 1] == 'x')
+		return ptf(s, args, i + 2, n + putnum(va_arg(args, int), 16));
+	write(1, &s[i], 1);
+	return ptf(s, args, i + 1, n + 1);
+}
+
+int ft_printf(char *s, ...)
+{
+	va_list args;
+	return va_start(args, s), va_end(args), ptf(s, args, 0, 0);
+}
